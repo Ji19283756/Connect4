f43c7a9f56e582fea7200ad0af2fcdfc1472c528
Ji19283756 <ji19283756@gmail.com>
2023-09-13 13:03:08 -0400

    added more functionality to the board class

--- Board.py
+++ Board.py
@@ -27,4 +27,46 @@ class Board:
     # will check the board for any winners, returns 0 if no one, and 1 or 2 depending on
     # the winner
     def check_for_winner(self) -> int:
+        def check_horizontal():
+            for row in self.board:
+                for col in range(len(row) - self.connect_length + 1):
+                    if all(row[col + i] == current_player for i in range(self.connect_length)):
+                        return current_player
+            return 0
 
+        def check_vertical():
+            for col in range(len(self.board[0])):
+                for row in range(len(self.board) - self.connect_length + 1):
+                    if all(self.board[row + i][col] == current_player for i in range(self.connect_length)):
+                        return current_player
+            return 0
+
+        def check_diagonal():
+            for row in range(len(self.board) - self.connect_length + 1):
+                for col in range(len(self.board[0]) - self.connect_length + 1):
+                    if all(self.board[row + i][col + i] == current_player for i in range(self.connect_length)):
+                        return current_player
+
+            for row in range(len(self.board) - self.connect_length + 1):
+                for col in range(self.connect_length - 1, len(self.board[0])):
+                    if all(self.board[row + i][col - i] == current_player for i in range(self.connect_length)):
+                        return current_player
+            return 0
+
+        for current_player in [1, 2]:
+            if check_vertical() or check_horizontal() or check_diagonal():
+                return current_player
+
+        return 0
+
+    # returns a list of open columns
+    def give_open_cols(self) -> list:
+        open_cols = []
+
+        # iterates through the columns of the board
+        for x_col in range(len(self.board[0])):
+            # if the top value is 0, then that means that there's still spaces
+            if self.board[0][x_col] == 0:
+                open_cols += [x_col]
+
+        return open_cols
